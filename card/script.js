d63bcfc519f77b9e49cc14b9942c9df0f2c9d881
rabbit0511 <130059037+rabbit0511@users.noreply.github.com>
2023-05-18 19:26:28 +0800

    Add files via upload

--- card/script.js
+++ card/script.js
@@ -1,33 +1,59 @@
 var selectedCards = [];
 var maxSelectedCards = 5;
-
+var imgFolder = "img";
+var ImgList = [
+    'ace-cups.jpg', 'ace-pentacles.jpg', 'ace-swords.jpg', 'ace-wands.jpg', 'chariot.jpg',
+    'death.jpg', 'devil.jpg', 'eight-cups.jpg', 'eight-pentacles.jpg', 'eight-swords.jpg',
+    'eight-wands.jpg', 'emperor.jpg', 'empress.jpg', 'five-cups.jpg', 'five-pentacles.jpg',
+    'five-swords.jpg', 'five-wands.jpg', 'fool.jpg', 'fortune-wheel.jpg', 'four-cups.jpg',
+    'four-pentacles.jpg', 'four-swords.jpg', 'four-wands.jpg', 'hanged-man.jpg', 'hermit.jpg',
+    'hierophant.jpg', 'high-priestess.jpg', 'judgement.jpg', 'justice.jpg', 'king-cups.jpg',
+    'king-pentacles.jpg', 'king-swords.jpg', 'king-wands.jpg', 'knight-cups.jpg', 'knight-pentacles.jpg',
+    'knight-swords.jpg', 'knight-wands.jpg', 'lovers.jpg', 'magician.jpg', 'moon.jpg', 'nine-cups.jpg',
+    'nine-pentacles.jpg', 'nine-swords.jpg', 'nine-wands.jpg', 'page-cups.jpg', 'page-pentacles.jpg',
+    'page-swords.jpg', 'page-wands.jpg', 'queen-cups.jpg', 'queen-pentacles.jpg', 'queen-swords.jpg',
+    'queen-wands.jpg', 'seven-cups.jpg', 'seven-pentacles.jpg', 'seven-swords.jpg', 'seven-wands.jpg',
+    'six-cups.jpg', 'six-pentacles.jpg', 'six-swords.jpg', 'six-wands.jpg', 'stars.jpg', 'strength.jpg',
+    'sun.jpg', 'temperance.jpg', 'ten-cups.jpg', 'ten-pentacles.jpg', 'ten-swords.jpg', 'ten-wands.jpg',
+    'three-cups.jpg', 'three-pentacles.jpg', 'three-swords.jpg', 'three-wands.jpg', 'tower.jpg',
+    'two-cups.jpg', 'two-pentacles.jpg', 'two-swords.jpg', 'two-wands.jpg', 'world.jpg']
 function flipCard(cardId, card) {
-
-
     if (selectedCards.length < maxSelectedCards || true) {
+        var RandomInt = getRandomInt(1, 2)
         var card = document.getElementById(cardId);
-        card.style.backgroundImage = `url(img/${cardId}.jpg)`;
-        selectedCards.push(cardId);
-        updateSelectedCards();
+        selectedCards.push({ cardId, RandomInt });
+        updateSelectedCards(RandomInt);
         card.parentNode.removeChild(card); // 移除被點擊的卡片
     } else {
         alert("已达到最大卡片数量！");
     }
 }
-
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max) + 1;
+    return Math.floor(Math.random() * (max - min) + min)
+}
 function updateSelectedCards() {
     var selectedCardsContainer = document.getElementById("selectedCards");
     selectedCardsContainer.innerHTML = "";
-
+    console.log(selectedCards);
     for (var i = 0; i < selectedCards.length; i++) {
-        var cardId = selectedCards[i];
+        var cardData = selectedCards[i];
+        var cardId = cardData.cardId;
+        var RandomInt = cardData.RandomInt;
+
         var cardElement = document.createElement("div");
         cardElement.className = "selectedCard";
-        cardElement.style.backgroundImage = `url(img/${cardId}.jpg)`;
+        cardElement.style.backgroundImage = `url(img${RandomInt}/${ImgList[cardId]})`;
+
+        var cardNameElement = document.createElement("div");
+        cardNameElement.className = "cardName";
+        cardNameElement.textContent = ImgList[cardId].replace(".jpg", "");
+
+        cardElement.appendChild(cardNameElement);
         selectedCardsContainer.appendChild(cardElement);
     }
 }
-
 function restoreCards() {
     selectedCards = [];
     updateSelectedCards();
@@ -53,8 +79,6 @@ function restoreCards() {
 
 function shuffleArray(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
-
-    // While there remain elements to shuffle
     while (currentIndex !== 0) {
         // Pick a remaining element
         randomIndex = Math.floor(Math.random() * currentIndex);
@@ -69,8 +93,8 @@ function shuffleArray(array) {
     return array;
 }
 var cardIds = [];
-for (var i = 1; i <= 54; i++) {
-    cardIds.push("card" + i);
+for (var i = 0; i < 78; i++) {
+    cardIds.push(i);
 }
 
 var shuffledCardIds = shuffleArray(cardIds);
@@ -83,9 +107,7 @@ for (var i = 0; i < shuffledCardIds.length; i++) {
     cardElement.style.left = (i * 20) + "px"; // 調整卡片水平間距
     cardElement.onclick = function () {
         flipCard(this.id, this);
-
     };
     cardElement.style.backgroundImage = "url(card_back.jpg)"; // 添加卡片背面图像
-    console.log(i)
     deckElement.appendChild(cardElement);
 }
